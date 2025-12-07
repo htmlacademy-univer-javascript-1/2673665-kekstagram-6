9acec7bf8e37e1959dadd38ae3c78393d0b60d02
Aigul <aigul56263@gmail.com>
2025-12-07 20:42:14 +0500

    подключение эффектов

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ import { generatePhotos } from './photos.js';
 import { renderThumbnail } from './thumbnails.js';
 import { openFullPhoto } from './full-photo.js';
 import { initForm } from './form.js';
+import { initScaleAndEffects } from './scale-and-effects.js';
 
 const userPhotos = generatePhotos();
 const picturesContainer = document.querySelector('.pictures');
@@ -10,8 +11,9 @@ renderThumbnail(userPhotos, picturesContainer);
 
 const initializeForm = () => {
   const form = document.querySelector('#upload-select-image');
-  if (form && typeof Pristine !== 'undefined') {
+  if (form && typeof Pristine !== 'undefined' && typeof noUiSlider !== 'undefined') {
     initForm();
+    initScaleAndEffects();
   } else {
     setTimeout(initializeForm, 100);
   }
